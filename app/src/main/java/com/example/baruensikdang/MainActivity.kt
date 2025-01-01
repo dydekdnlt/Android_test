df92d2d18e165126aea69a0fdf1f7c50a27bd06e
dydekdnlt <dydekdnlt071@naver.com>
2025-01-01 17:46:28 +0900

    Test git connect

--- app/src/main/java/com/example/baruensikdang/MainActivity.kt
+++ app/src/main/java/com/example/baruensikdang/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
+// git connect test
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
